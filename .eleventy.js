3c4454596c6080792e1754a3ea85348af5f791ff
Gilro <gilron31@gmail.com>
2024-04-28 22:13:27 +0300

    more transfer

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const mathjaxPlugin = require('eleventy-plugin-mathjax');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/css/style.css');
   eleventyConfig.addPassthroughCopy('src/js/script.js');
   eleventyConfig.addPassthroughCopy('**/*.pdf');
+  eleventyConfig.addPlugin(mathjaxPlugin);
+
   return {
     dir: {
       input: 'src',
